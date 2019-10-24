30ea31392603467a73cd288c593dccb69d52492b
Nina Liu <nina.liu@appian.com>
2019-10-24 13:18:12 -0400

    add mockData

--- app/components/TaskItem.js
+++ app/components/TaskItem.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { tasks } from "../mockData/mockTask";
 
 class TaskItem extends Component {
+
     render() {
-        return <Text>just a test</Text>
+        const item = tasks[0];
+        return (
+            <View style={styles.taskContainer} >
+                <Text>{item.displayName}</Text>
+                <View>
+                    <Text>{item.assignees[0].displayName}</Text>
+                </View>
+            </View>
+        )
     }
 }
-
+const styles = StyleSheet.create({
+    taskContainer: {
+        
+    }
+})
 export default TaskItem
---
+++ app/mockData/mockTask.js
@@ -0,0 +1,25 @@
+export const tasks = [{
+         "id":2169063,
+         "displayName":"Default ",
+         "priority":{
+            "id":1,
+            "name":"Normal"
+         },
+         "deadline":null,
+         "assignedTime":"2019-10-24T15:39:34.720Z",
+         "assignees":[
+            {
+               "id":"nina.liu.d",
+               "displayName":"Nina Liu Designer",
+               "isUser":true
+            }
+         ],
+         "favorite":false,
+         "overdue":false,
+         "legacyForm":false,
+         "autoDownloadable":true,
+         "acceptLink":"https://mobile-daily.appianci.net/suite/rest/a/task/latest/2169063/accept",
+         "formUiLink":"https://mobile-daily.appianci.net/suite/rest/a/task/latest/2169063/form",
+         "attributesLink":"https://mobile-daily.appianci.net/suite/rest/a/task/latest/2169063/attributes",
+         "statusLink":"https://mobile-daily.appianci.net/suite/rest/a/task/latest/2169063/status"
+      }]
