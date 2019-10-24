2352e0fa97c5d0c13959db3109279d2993889b8d
Kavin Kasinathan <kavin.kasinathan@appian.com>
2019-10-24 15:29:41 -0400

    Get Data from server and load initail view

--- app/App.js
+++ app/App.js
@@ -6,14 +6,16 @@
  * @flow
  */
 
-import React from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
   View,
+  Text,
   FlatList
 } from 'react-native';
 import TaskItem from './components/TaskItem';
 import {data} from './mockData/mockData';
+import {getTasks} from './services/TaskService';
 import {
   Header,
   LearnMoreLinks,
@@ -22,14 +24,34 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const tasks = data.tasks;
+class App extends Component {
+  constructor() {
+    super();
+    getTasks()
+      .then(response => response.json())
+      .then(jsonResponse => {
+        this.setState({data: jsonResponse});
+      })
+      .catch(error => {
+        console.log(`Error while fetching tasks`);
+      });
+  }
+  state = {data: undefined};
 
-const App: () => React$Node = () => {
-  return (
-    <View style={styles.appContainer} >
-    <FlatList data={tasks} renderItem={({item}) => <TaskItem task={item}/>}/>
-    </View>
-  );
+  render() {
+    if (!this.state.data) {
+      return (
+        <View style={styles.appContainer}>
+          <Text>No items in list</Text>
+        </View>
+      );
+    }
+    return (
+      <View style={styles.appContainer}>
+        <FlatList data={this.state.data.tasks} renderItem={({item}) => <TaskItem task={item}/>}/>
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
