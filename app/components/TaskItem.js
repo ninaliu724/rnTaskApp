26a35fb1adda07a41ee18f1952bd7200b636ed44
Nina Liu <nina.liu@appian.com>
2019-10-24 11:34:47 -0400

    add TaskItem

---
+++ app/components/TaskItem.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class TaskItem extends Component {
+    render() {
+        return <Text>just a test</Text>
+    }
+}
+
+export default TaskItem
